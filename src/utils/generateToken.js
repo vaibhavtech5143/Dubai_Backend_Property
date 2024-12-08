617255eae3947d2aa628f77ac017948217c08513
Vaibhav Singh <vaibhavsingh2633@gmail.com>
2024-12-08 17:56:17 +0530

    res json sent of cookie

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -11,6 +11,7 @@ const generateToken = (res, userId) => {
     sameSite: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
+  res.json({ token });
 };
 
 module.exports = generateToken;
